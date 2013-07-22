8537145888a5d772fbafccfeae0d28ccc4ce534c
Steve Ratcliffe <sr@itinken.com>
2013-07-22 11:22:59 +0100

    Add some tests that would have caught the reported bug.

--- tests/placeholder.js
+++ tests/placeholder.js
@@ -1,7 +1,11 @@
 
 (function() {
-
-    module('placeholder');
+	var span;
+    module('placeholder', {
+		setup: function() {
+			span = $('<span data-placeholder="HI"></span>');
+		}
+	});
 
     var $qfix = $("#qunit-fixture");
 
@@ -19,8 +23,7 @@
         equal($(e).text(), "HI");
     });
 
-    test("nil as placeholder fallback", function () {
-        expect(3);
+    test("nil as placeholder fallback", 3, function () {
 
         $qfix.append("<span id='E' data-nil='HI'></span>");
         var e = $('#E').jinplace().get(0);
@@ -34,5 +37,29 @@
         e = $('#G').jinplace({nil: 'GG'}).get(0);
         equal($(e).text(), "HI");
     });
+
+	test('Empty element contains placeholder', 1, function() {
+		span.jinplace();
+
+		equal(span.text(), 'HI');
+	});
+
+	test('Placeholder not present when editing', 2, function() {
+		span.jinplace();
+		span.click();
+		equal(span.find(':input').val(), '');
+		equal(span.find(':input').text(), '');
+	});
+
+	test('Placeholder applied when empty text received', 2, function() {
+		span.jinplace();
+		span.click();
+
+		span.find(':input').val('');
+		span.find('form').trigger('jip:submit');
+
+		equal(span.find(':input').length, 0);
+		equal(span.text(), 'HI');
+	});
 
 })();
---
+++ tests/text_only.js
@@ -0,0 +1,56 @@
+(function() {
+	var div, to_div;
+
+	module('text_only option', {
+		setup:function() {
+			div = $('<div data-type="textarea"><ul><li>Hello</ul></div>');
+		}
+	});
+
+	function submit() {
+		div.find('form').trigger('jip:submit');
+	}
+
+	test('With text only returned html is displayed as text', 2, function() {
+		div.jinplace();
+
+		div.click();
+
+		var textarea = div.find(':input');
+		var orig = textarea.val();
+		textarea.val(orig.replace(/Hello/, 'Hello<li>World'));
+		submit();
+
+		ok(div.text().indexOf('World') > 0, 'text changed');
+		ok(div.text().indexOf('<ul>') >= 0, 'literal html');
+	});
+
+	test('With textOnly returned html is displayed as text', 2, function() {
+		div.jinplace();
+
+		div.click();
+
+		var textarea = div.find(':input');
+		var orig = textarea.val();
+		textarea.val(orig.replace(/Hello/, 'Hello<li>World'));
+		submit();
+
+		ok(div.text().indexOf('World') > 0, 'text changed');
+		ok(div.text().indexOf('<ul>') >= 0, 'literal html');
+	});
+
+	test('With textOnly==false returned html is interpreted', 2, function() {
+		div.attr('data-text-only', 'false');
+		div.jinplace();
+
+		div.click();
+
+		var textarea = div.find(':input');
+		var orig = textarea.val();
+		textarea.val(orig.replace(/Hello/, 'Hello<li>World'));
+		submit();
+
+		ok(div.text().indexOf('World') > 0, 'text changed');
+		ok(div.text().indexOf('<ul>') == -1, 'literal html');
+	});
+})();
