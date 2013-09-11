92f20c3c72d52646ad8eac3c86ae88499ed07796
Steve Ratcliffe <sr@itinken.com>
2013-09-11 18:54:32 +0100

    JIN-36 Android/Opera focus test failures.
    Turns out this was a test bug, not a jinplace bug.

--- tests/focus.js
+++ tests/focus.js
@@ -10,11 +10,33 @@
 		}
 	});
 
+	/**
+	 * There is a problem with using :focus on Android browser and IE6-8. The document.hasFocus() always
+	 * returns false (at least under the conditions of the test) and so :focus does too. So we do
+	 * our own checks only check for the active element.
+	 *
+	 * @param $el The start element, we search for a focused element underneath this.
+	 * @returns {jQuery} The focused element, or an empty jquery object.
+	 */
+	var findFocused = function($el) {
+		var result = $();
+
+		$el.find('*').each(function() {
+			if (this === document.activeElement) {
+				result = $(this);
+				return false;
+			}
+			return true;
+		});
+
+		return result;
+	};
+
 	test('focus for text input type', 1, function() {
 		span.attr('data-type', 'input');
 		span.jinplace().click();
 
-		var focused = span.find(':focus').first();
+		var focused = findFocused(span);
 		equal(focused.prop('tagName'), 'INPUT');
 	});
 
@@ -22,7 +44,7 @@
 		span.attr('data-type', 'textarea');
 		span.jinplace().click();
 
-		var focused = span.find(':focus').first();
+		var focused = findFocused(span);
 		equal(focused.prop('tagName'), 'TEXTAREA');
 	});
 
@@ -31,7 +53,7 @@
 		span.attr('data-data', '[[1,"A"],[2,"B"]]');
 		span.jinplace().click();
 
-		var focused = span.find(':focus').first();
+		var focused = findFocused(span);
 		equal(focused.prop('tagName'), 'SELECT');
 	});
 
